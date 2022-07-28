cbbfaec6a17714f69856de8ddee874508b77fd5e
DESKTOP-RD1DKE2\zaqxs <zaqxswcdevfrbgtnhymjukilope@gmail.com>
2022-07-28 20:11:03 +0900

    modify image url

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from skimage.io import imread
 
 
 # download the image
-img_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Mount_Fuji_from_Mount_Aino.jpg/640px-Mount_Fuji_from_Mount_Aino.jpg'
+img_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/File:K2_2006b.jpg/640px-File:K2_2006b.jpg'
 
 im = imread(img_url)
 
