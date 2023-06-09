f277b81e3d3e7507ca721e6dc8ad5b77d1852e69
Toru Tamaki <tamaki.toru@nitech.ac.jp>
2023-06-09 14:05:34 +0900

    fix path in start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-echo "[Debug]"
-echo "this is /tmp/start.sh"
-ls -la /tmp
 
-streamlit run /tmp/main.py --server.port=${PORT} --server.address=0.0.0.0
+streamlit run /root/main.py --server.port=${PORT} --server.address=0.0.0.0
