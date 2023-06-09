8cd0d7b0a108edd14520e83c088b0418e860937f
Toru Tamaki <tamaki.toru@nitech.ac.jp>
2023-06-09 15:15:49 +0900

    add extra brace by an error

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from skimage.io import imread
 # download the image
 img_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Mount_Fuji_from_Mount_Aino.jpg/640px-Mount_Fuji_from_Mount_Aino.jpg'
 
-im = imread(img_url)
+im = imread(img_url))
 
 st.image(im, caption='image from wikimedia commons',
          use_column_width=True)
