11f3032fc453ff42e824d3898cfacb9737e6607b
kenken056610 <135195480+kenken056610@users.noreply.github.com>
2023-06-19 14:46:46 +0900

    fix

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ img_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Mount_Fuji_
 
 im = imread(img_url)
 
-st.image(im, caption='image from wikimedia commons',
+st.image(im, caption='image from from wikimedia commons',
          use_column_width=True)
 
 
